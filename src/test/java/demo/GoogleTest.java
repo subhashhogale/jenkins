3d1f35dc5150a3f4c1fa2f4d0700a43e78ec8a86
subhashhogale <hogalesubhash2@gmail.com>
2026-04-15 03:01:02 +0530

    Update GoogleTest.java
    new

--- src/test/java/demo/GoogleTest.java
+++ src/test/java/demo/GoogleTest.java
@@ -15,7 +15,7 @@ public class GoogleTest {
         driver.get("https://www.google.com");
 
         System.out.println("Title is: " + driver.getTitle());
-          System.out.println("subhashhogale");
+          System.out.println("subhashhogalesangavi");
         driver.quit();
     }
 }
